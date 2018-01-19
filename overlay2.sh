0c52f9b63076d6e23726aac569764055439d07e5
Chenzhu1008 <c54948054@126.com>
2018-01-19 14:29:19 +0800

    Update overlay2.sh

--- overlay2.sh
+++ overlay2.sh
@@ -1,6 +1,42 @@
 #!/usr/bin/env bash
+#镜像仓库地址
+registryAdd="100.125.5.235:20202"
+
+#运行场景: 类生产为"prepare"; 生产环境为"formally"
+runtimeScene="prepare"
+
+#euler内核版本：一般不需修改
+eulerOSKernel="3.10.0-327.44.58.35.x86_64"
+
+echo '
+######################关于配置项的说明######################
+
+镜像仓库：     registryAdd="'${registryAdd}'"
+运行场景：     runtimeScene="'${runtimeScene}'"    ###类生产为"prepare"; 生产环境为"formally"
+euler内核版本：eulerOSKernel="'${eulerOSKernel}'"  ###一般不需要配置
+
+可通过命令行参数传入: bash overlay2.sh {registryAdd} {runtimeScene} {eulerOSKernel}
+参数可由后至前依次省略，省略项则使用以上对应的默认值
+
+##########################################################'
+
+if [ $# -ge 1 ]; then
+    if [[ $1 == "-h" || $1 == "--help" ]]; then
+       exit 0
+    fi
+    registryAdd=$1
+    if [ $# -ge 2 ]; then
+        if [ $2 == "formally" ]; then
+            runtimeScene="formally"
+        fi
+        if [ $# -ge 3 ]; then
+            eulerOSKernel=$3
+        fi
+    fi
+fi
+
 #overlay2 modules
-overlayKo="/lib/modules/3.10.0-327.44.58.35.x86_64/kernel/fs/overlayfs/overlay.ko"
+overlayKo="/lib/modules/${eulerOSKernel}/kernel/fs/overlayfs/overlay.ko"
 modulesLoad="/etc/modules-load.d"
 dockerOverlay="${modulesLoad}/overlay.conf"
 
@@ -9,14 +45,12 @@ dockerConfDir="/etc/docker"
 dockerDaemon="${dockerConfDir}/daemon.json"
 
 #docker proxy
-dockerServiceDir="./etc/systemd/system/docker.service.d"
+dockerServiceDir="/etc/systemd/system/docker.service.d"
 dockerProxy="${dockerServiceDir}/http-proxy.conf"
 
 #docker mirror
 dockerServiceLib="/lib/systemd/system/docker.service"
 dockerServiceEtc="/etc/systemd/system/docker.service"
-execStart="ExecStart=/usr/bin/docker daemon"
-registry_mirror="--registry-mirror=https://100.125.5.235:20202"
 
 
 needUpdateOverlay=0
@@ -53,33 +87,36 @@ if [ ${needUpdateDaemon} -eq 0 ]; then
     echo '}' >> ${dockerDaemon}
 fi
 
-needUpdateService=0
-if [ ! -d "${dockerServiceDir}" ]; then
-    mkdir -p  ${dockerServiceDir}
-fi
+#生产环境pod域直通外网，不需要设置代理
+if [ ${runtimeScene} != "formally" ]; then
+    needUpdateService=0
+    if [ ! -d "${dockerServiceDir}" ]; then
+        mkdir -p  ${dockerServiceDir}
+    fi
 
-if [ -f "${dockerProxy}" ]; then
-   needUpdateService=`cat "${dockerProxy}" | grep '10.177.221.12:3128' | wc -l`
-fi
+    if [ -f "${dockerProxy}" ]; then
+       needUpdateService=`cat "${dockerProxy}" | grep '10.177.221.12:3128' | wc -l`
+    fi
 
-if [ ${needUpdateService} -eq 0 ]; then
-    #set docker proxy
-    echo '[Service]' >> ${dockerProxy}
-    echo 'Environment="HTTP_PROXY=10.177.221.12:3128" "HTTPS_PROXY=10.177.221.12:3128" "NO_PROXY=100.125.5.235"' >> ${dockerProxy}
+    if [ ${needUpdateService} -eq 0 ]; then
+        #set docker proxy
+        echo '[Service]' >> ${dockerProxy}
+        echo 'Environment="HTTP_PROXY=10.177.221.12:3128" "HTTPS_PROXY=10.177.221.12:3128" "NO_PROXY=100.125.5.235"' >> ${dockerProxy}
+    fi
 fi
 
 needUpdateMirror=0
 if [ -f "${dockerServiceEtc}" ]; then
-   needUpdateMirror=`cat "${dockerServiceEtc}" | grep "${registry_mirror}" | wc -l`
+   needUpdateMirror=`cat "${dockerServiceEtc}" | grep "registry-mirror" | grep "${registryAdd}" | wc -l`
 else
-   needUpdateMirror=`cat "${dockerServiceLib}" | grep "${registry_mirror}" | wc -l`
+   needUpdateMirror=`cat "${dockerServiceLib}" | grep "registry-mirror" | grep "${registryAdd}" | wc -l`
    if [ ${needUpdateMirror} -eq 0 ]; then
         cp -n ${dockerServiceLib} ${dockerServiceEtc}
    fi
 fi
 
 if [ ${needUpdateMirror} -eq 0 ]; then
-    sed -i "s|${execStart}|${execStart} ${registry_mirror}" ${dockerServiceEtc}
+    sed -i 's/docker daemon/docker daemon --registry-mirror=https:\/\/'${registryAdd}'/g' ${dockerServiceEtc}
 fi
 
 #restat docker daemon
